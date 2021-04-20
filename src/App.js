c4143ad5fd1e383c5c9d2dc41968d77d435e86f2
Cole Winslow <colethomaswinslow7@gmail.com>
2021-04-20 14:32:40 -0700

    removed starter code

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+// import HelloBootstrap from "./components/helloBootstrap.js";
+function App() {
+  return (
+/* The Components Needed
+<Nav/> 
+<
+
+*/
+
+  );
+}
+
+export default App;
