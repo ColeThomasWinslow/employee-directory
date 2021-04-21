8c8898bfb5119dd9c85acd1dd13a6749deec3bcc
Cole Winslow <colethomaswinslow7@gmail.com>
2021-04-21 14:44:43 -0700

    added axious

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
 import React from "react";
-import Header from "./components/Header";
-import Search from "./components/Search";
-import TableTitles from "./components/TableTitles";
-import Footer from "./components/Footer";
-// import SearchBar from "./components/Search";
-function App() {
-  return (<div>
- <Header/>
-  <Search/>
- <TableTitles/>
- <Footer/>
-</div>
-   
+import API from "./utils/API";
 
-  );
+// import SearchBar from "./components/Search";
+
+
+
+class App extends React.Component {
+  state = {
+    employees: [],
+  };
+
+  componentDidMount() {
+    this.getEmployees();
+  }
+
+  getEmployees = async () => {
+    const { data } = await API.getUsers();
+    this.setState({ employees: data.results });
+  };
+
+  render() {
+    console.log(this.state);
+    return <h1>Employees go here</h1>;
+  }
+  render() {
+    return (
+      <div>
+       <SearchResultContainer />
+      </div>
+    );
+  }
 }
 
 export default App;
